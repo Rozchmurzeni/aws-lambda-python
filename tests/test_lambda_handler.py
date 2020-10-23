d221fd521e0d1da4ce4741a4197a4769ffcc2bf7
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-10-23 13:08:13 +0200

    Correct test and gitignore

--- tests/test_lambda_handler.py
+++ tests/test_lambda_handler.py
@@ -1,13 +1,17 @@
 import unittest
+import sys
+import os
 from calculate_student_grade.lambda_handler import handle
 
+sys.path.insert(0, os.path.abspath('..'))
 
-class TestIsStatusProperToCheckDrift(unittest.TestCase):
+
+class TestReturnCorrectResponse(unittest.TestCase):
     def test_handle(self):
-        result = handle()
+        response = handle(None, None)
 
-        self.assertEqual(result.body, 5)
-        self.assertEqual(result.statusCode, 200)
+        self.assertEqual(response['body'], 5)
+        self.assertEqual(response['statusCode'], 200)
 
 
 if __name__ == '__main__':
