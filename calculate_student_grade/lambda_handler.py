a673092a4068efe8f19da1135f47f5d7476ea196
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2021-02-11 23:17:08 +0100

    Simplify calculation invocation

--- calculate_student_grade/lambda_handler.py
+++ calculate_student_grade/lambda_handler.py
@@ -5,8 +5,8 @@ def handle(event, context):
     return produce_result(calculate, event['lectureGrade'], event['exerciseGrade'], event['workshopGrade'])
 
 
-def produce_result(calculate_function, lecture_grade, exercise_grade, workshop_grade):
-    final_grade, has_passed = calculate_function(lecture_grade, exercise_grade, workshop_grade)
+def produce_result(lecture_grade, exercise_grade, workshop_grade):
+    final_grade, has_passed = calculate(lecture_grade, exercise_grade, workshop_grade)
     return {
         'finalGrade': final_grade,
         'hasPassed': has_passed
